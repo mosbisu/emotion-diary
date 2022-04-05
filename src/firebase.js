8c0e3a528bf33771f7e01415a9266ff22bc7a4df
kr <hoylfree@gmail.com>
2022-04-06 00:35:43 +0900

    firebase settings

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCMhskq9Jt0E4mGg8_vyZ0AYWt2TIZBL0",
+  authDomain: "emotion-diary-fc6ff.firebaseapp.com",
+  projectId: "emotion-diary-fc6ff",
+  storageBucket: "emotion-diary-fc6ff.appspot.com",
+  messagingSenderId: "654463775708",
+  appId: "1:654463775708:web:ee96ae7790c7809a36d519",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const authService = getAuth(app);
+export const dbService = getFirestore();
+export const storageService = getStorage();
